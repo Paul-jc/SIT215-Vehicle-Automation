31b0bf69ed89c27d0f6b581ea6f73d96f5c1d750
Paul.Clarke <paul.clarke87@gmail.com>
2022-03-26 19:19:01 +1100

    basic structure implemented

--- car.py
+++ car.py
@@ -1,19 +1,18 @@
 class Car:
-    def __init__(self, vehicle_mass, current_speed, desired_speed, accelleration_percent, braking_percent):
-        self.vehicle_mass = vehicle_mass
+    def __init__(self, current_speed: float, desired_speed: int, accelleration_percent: float, braking_percent: float, max_acc: float, max_brake: float, seconds_driving: float):
         self.current_speed = current_speed
         self.desired_speed = desired_speed
         self.accelleration_percent = accelleration_percent
         self.braking_percent = braking_percent
+        self.max_acc = max_acc
+        self.max_brake = max_brake
+        self.seconds_driving = seconds_driving
 
     def accelerate(self):
         self.current_speed += self.accelleration_percent
     
     def brake(self):
         self.current_speed -= self.braking_percent
-    
-    def get_vehicle_mass(self):
-        return self.vehicle_mass
 
     def get_current_speed(self):
         return self.current_speed
@@ -27,8 +26,14 @@ class Car:
     def get_braking_percent(self):
         return self.braking_percent
 
-    def set_vehicle_mass(self, vehicle_mass):
-        self.vehicle_mass = vehicle_mass
+    def get_max_acc(self):
+        return self.max_acc
+    
+    def get_max_brake(self):
+        return self.max_brake
+    
+    def get_seconds_driving(self):
+        return self.seconds_driving
     
     def set_current_speed(self, current_speed):
         self.current_speed = current_speed
@@ -42,4 +47,12 @@ class Car:
     def set_braking_percent(self, braking_percent):
         self.braking_percent = braking_percent
 
+    def set_max_acc(self, max_acc):
+        self.max_acc = max_acc
+    
+    def set_max_brake(self, max_brake):
+        self.max_brake = max_brake
+
+    def increment_seconds_driving(self, seconds_driving):
+        self.seconds_driving += seconds_driving
     
--- driving.py
+++ driving.py
@@ -2,3 +2,94 @@ import car
 import visualisation
 
 # TODO - implement vehicle control logic
+
+
+# Convert km/h to m/s for sake of calcuatations
+def kmh_to_ms(kmh):
+    return kmh / 3.6
+
+
+# Convert m/s to km/h for sake of output
+def ms_to_kmh(ms):
+    return ms * 3.6
+
+
+# Calculate speed difference between current and desired speed
+def calculate_speed_difference(current_speed, desired_speed):
+    return desired_speed - current_speed
+
+
+def set_braking_percent(car_object):
+    if car_object.get_current_speed() > 0:
+        braking_per = abs(calculate_speed_difference(car_object.get_current_speed(
+        ), car_object.get_desired_speed()) / car_object.get_current_speed())/10
+    else:
+        braking_per = 0
+    car_object.set_braking_percent(braking_per)
+
+
+def set_acc_percent(car_object):
+    if car_object.get_current_speed() > 0:
+        acc_per = abs(calculate_speed_difference(car_object.get_current_speed(
+        ), car_object.get_desired_speed()) / car_object.get_current_speed())/10
+    else:
+        acc_per = 0.15
+    car_object.set_accelleration_percent(acc_per)
+
+
+# While the car is not at desired speed, accelerate or brake
+def drive_car(car_object):
+    while calculate_speed_difference(car_object.get_current_speed(), car_object.get_desired_speed()) > 0.5:
+        set_acc_percent(car_object)
+        print(f"Accelerating at: {car_object.get_accelleration_percent()}")
+        accelerate(car_object)
+        car_object.increment_seconds_driving(1)
+        # visualisation.update_car_speed(car_object)
+        print(f"Current speed: {str(car_object.get_current_speed())}")
+    while calculate_speed_difference(car_object.get_current_speed(), car_object.get_desired_speed()) < -0.5:
+        print(calculate_speed_difference(
+            car_object.get_current_speed(), car_object.get_desired_speed()))
+        set_braking_percent(car_object)
+        print(f"Braking at: {car_object.get_braking_percent()}")
+        decellerate(car_object)
+        car_object.increment_seconds_driving(1)
+        # visualisation.update_car_speed(car_object)
+        print(f"Current speed: {str(car_object.get_current_speed())}")
+
+
+# accelerate car to desired speed at maximum 2.7 m/s
+def accelerate(car_object):
+    car_object.set_current_speed(
+        ms_to_kmh(
+            kmh_to_ms(car_object.get_current_speed()) + kmh_to_ms(car_object.get_accelleration_percent() * car_object.get_max_acc()))
+    )
+
+
+# decellerate car to desired speed at maximum 29 m/s
+def decellerate(car_object):
+    car_object.set_current_speed(
+        ms_to_kmh(
+            kmh_to_ms(car_object.get_current_speed()) - kmh_to_ms(car_object.get_braking_percent() * car_object.get_max_brake()))
+    )
+
+
+def main():
+    drivers_car = car.Car(
+        current_speed=0,
+        desired_speed=100,
+        accelleration_percent=0,
+        braking_percent=0,
+        max_acc=2.7,
+        max_brake=29,
+        seconds_driving=0
+    )
+    print(f"Current speed: {str(drivers_car.get_current_speed())}")
+    print(
+        f"Current speed in m/s: {str(kmh_to_ms(drivers_car.get_current_speed()))}")
+    drive_car(drivers_car)
+    print(f"Current speed: {str(drivers_car.get_current_speed())}")
+    print(f"Seconds driving: {str(drivers_car.get_seconds_driving())}")
+
+
+if __name__ == "__main__":
+    main()
